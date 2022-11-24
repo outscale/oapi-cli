cb7b640b84ba68dd677ddbeb18aceb7b80593198
Matthias Gatto <matthias.gatto@outscale.com>
2022-11-24 17:59:39 +0100

    tests: add some tests
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- tests.sh
+++ tests.sh
@@ -2,7 +2,7 @@
 
 set -eE
 
-MSG_BASE="Test "
+MSG_BASE="Test"
 
 trap "echo [$MSG_BASE make FAIL]" ERR
 make
@@ -12,6 +12,10 @@ trap "echo [$MSG_BASE --help FAIL]" ERR
 ./oapi-cli --help > /dev/null
 echo "[$MSG_BASE --help OK]"
 
+trap "echo [$MSG_BASE --help ReadVms]" ERR
+./oapi-cli --help ReadVms | grep Filters > /dev/null
+echo "[$MSG_BASE --help ReadVms OK]"
+
 trap "echo [$MSG_BASE ReadVms FAIL]" ERR
 ./oapi-cli ReadVms | grep VmId > /dev/null
 echo "[$MSG_BASE ReadVms OK]"
@@ -19,3 +23,11 @@ echo "[$MSG_BASE ReadVms OK]"
 trap "echo [$MSG_BASE ReadImages --DryRun FAIL]" ERR
 ./oapi-cli ReadImages --DryRun | grep RequestId > /dev/null
 echo "[$MSG_BASE ReadImages --DryRun  OK]"
+
+trap "echo [$MSG_BASE ReadImages --DryRun false FAIL]" ERR
+./oapi-cli ReadImages --DryRun false | grep ImageId > /dev/null
+echo "[$MSG_BASE ReadImages --DryRun false  OK]"
+
+trap "echo [$MSG_BASE ReadImages --Filters.AccountAliases[] Outscale FAIL]" ERR
+./oapi-cli ReadImages --Filters.AccountAliases[] Outscale | grep ImageId > /dev/null
+echo "[$MSG_BASE ReadImages --Filters.AccountAliases[] Outscale OK]"
