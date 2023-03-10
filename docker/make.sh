f6ec3cdb801d30db9e721b959641850531523c95
Matthias Gatto <matthias.gatto@outscale.com>
2023-03-10 17:05:55 +0000

    is this final fix \? rust b***** edition
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- docker/make.sh
+++ docker/make.sh
@@ -2,8 +2,13 @@
 #to remove:
 #end to remove
 
+cd oapi-cli/
+make json-c/.git
+cd ..
+
 if [ -n "$1" ]; then
     if [ "$1" = "cp" ]; then
+	rm -rvf build/
 	mkdir build
 	cp -rvf oapi-cli/* ./build
     else
@@ -17,40 +22,51 @@ else
     git clean -df
 fi
 
-export JSON_C_WTF=/build/json-c/
-
 cd build/
-
-cmake --version
-
-make json-c/.git
 
 mkdir json-c-build
 cd json-c-build
 echo CMAKE NOW
-
 cmake ../json-c/
 cd ..
 
-git clone https://github.com/curl/curl.git
+git clone https://github.com/rustls/rustls-ffi -b v0.9.2
+cd rustls-ffi
+export PATH=$PATH:/${HOME}/.cargo/bin
+make
+make DESTDIR=${HOME}/rustls-ffi-built/ install
+cd ..
+
+git clone https://github.com/curl/curl.git -b curl-7_88_1
 cd curl
+
 autoreconf -fi
-./configure --with-openssl
+./configure --with-rustls=${HOME}/rustls-ffi-built --without-nss
 make
 cd ..
 
-cp -v curl/lib/.libs/libcurl.so .
+cp -v curl/lib/.libs/libcurl.so* .
 
 pwd
 
-echo CURL_LD="-L./ -lcurl" > config.mk
-echo LD_LIB_PATH+=":./" >> config.mk
+echo CURL_LD="-L$PWD -lcurl" > config.mk
+echo LD_LIB_PATH="$PWD/json-c-build:$PWD/curl/lib/.libs/" >> config.mk
 
 export CURL_BASH_CFLAGS=-Icurl/include/
 
 echo "do the make \!\!"
 make -C json-c-build json-c-static || 1 # ignore example compilation error
 
-rm -rvf /oapi-cli/docker-package/
+make
+echo "some ldds"
+echo "oapi-clip"
+LD_LIBRARY_PATH=$LD_LIB_PATH ldd oapi-cli
+echo "libcurl.so"
+LD_LIBRARY_PATH=$LD_LIB_PATH ldd libcurl.so
 make oapi-cli-x86_64.AppImage
 
+rm -rvf /oapi-cli/docker-package/
+mkdir /oapi-cli/docker-package/
+cp oapi-cli-x86_64.AppImage /oapi-cli/docker-package/
+cp /etc/pki/tls/certs/ca-bundle.crt /oapi-cli/docker-package/
+cp lslog /oapi-cli/docker-package/
