21a1a4d73c701b586ad03e87d7f905d5fccb381c
Matthias Gatto <matthias.gatto@outscale.com>
2023-04-28 10:21:54 +0200

    build auto-make
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- .github/script/release-make.sh
+++ .github/script/release-make.sh
@@ -11,4 +11,4 @@ github_url="https://api.github.com/repos/outscale/osc-api/releases"
 osc_api_last_release=$(curl -s -H "Authorization: token $GH_TOKEN" $github_url | jq ".[] | select(.prerelease == false) | select(.draft == false) | .tag_name" -r | sort -r --version-sort | head -n 1)
 echo "$osc_api_last_release" > $root/api_version
 
-COGNAC_CONFIG="--target-api=${osc_api_last_release}"  make -j -C $root/ regen
+COGNAC_CONFIG="--target-api=${osc_api_last_release}"  make -C $root/ gen
