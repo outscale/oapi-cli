fed952ea32e78abf6fdb943425db3e5e366d42b6
Matthias Gatto <matthias.gatto@outscale.com>
2023-06-16 15:45:01 +0200

    update osc-sdk-c at realease
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- .github/scripts/release-make.sh
+++ .github/scripts/release-make.sh
@@ -11,4 +11,10 @@ github_url="https://api.github.com/repos/outscale/osc-api/releases"
 osc_api_last_release=$(curl -s -H "Authorization: token $GH_TOKEN" $github_url | jq ".[] | select(.prerelease == false) | select(.draft == false) | .tag_name" -r | sort -r --version-sort | head -n 1)
 echo "$osc_api_last_release" > $root/api_version
 
+cd $root/osc-sdk-C
+git fetch origin
+git checkout origin/master
+cd -
+git add $root/osc-sdk-C/
+
 COGNAC_CONFIG="--yq-go --target-api=${osc_api_last_release}"  make -C $root/ gen
