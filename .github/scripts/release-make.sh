46da13054c707fa74cb413f5781e728c9557acd3
Matthias Gatto <matthias.gatto@outscale.com>
2023-06-19 10:49:26 +0200

    debug for CI
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- .github/scripts/release-make.sh
+++ .github/scripts/release-make.sh
@@ -11,6 +11,7 @@ github_url="https://api.github.com/repos/outscale/osc-api/releases"
 osc_api_last_release=$(curl -s -H "Authorization: token $GH_TOKEN" $github_url | jq ".[] | select(.prerelease == false) | select(.draft == false) | .tag_name" -r | sort -r --version-sort | head -n 1)
 echo "$osc_api_last_release" > $root/api_version
 
+set -x
 cd $root/osc-sdk-C
 git fetch origin
 git checkout origin/master
@@ -19,5 +20,6 @@ git add $root/osc-sdk-C/
 cp $root/osc-sdk-C/osc_sdk.c $root/
 cp $root/osc-sdk-C/osc_sdk.h $root/
 git add $root/osc_sdk.h $root/osc_sdk.c
+set +x
 
 COGNAC_CONFIG="--yq-go --target-api=${osc_api_last_release}"  make -C $root/ gen
