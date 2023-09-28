fa433544f7379de6b6b64503cdb7585b5d345381
Matthias Gatto <matthias.gatto@outscale.com>
2023-09-28 15:38:57 +0200

    tests: add more tests
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

---
+++ tests-lot-arg.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+./oapi-cli ReadVms --DryRun true --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true \
+	   --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true \
+	   ReadVms --DryRun true --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false \
+	   --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun false --DryRun true --DryRun true --DryRun true \
+	   --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true \
+	   --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true \
+	   --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true \
+	   --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true \
+	   --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true --DryRun true \
+	   --DryRun true --DryRun true
